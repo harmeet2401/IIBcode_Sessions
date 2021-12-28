bde0f5bd49addfe1e6f34dfbe262342e43e04029
harmeet2401 <harmeet2401@gmail.com>
2021-12-29 01:50:55 +0530

    .

---
+++ JavaUdemy/src/Arrays/ArrayClass.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+import java.util.ArrayList;
+
+public class ArrayClass {
+    public static void main(String[] args) {
+        int x = 1;
+        int a[] = {1,2,3};
+        System.out.println(a.length);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+        System.out.println("----------------------------------");
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.forEach(item -> System.out.println(item));
+        int y = () -> x++;
+    }
+}
