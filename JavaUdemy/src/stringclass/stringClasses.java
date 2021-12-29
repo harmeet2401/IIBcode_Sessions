08070c54bce93489cffe66e004076631f4552f71
harmeet2401 <harmeet2401@gmail.com>
2021-12-29 17:43:17 +0530

    .

---
+++ JavaUdemy/src/stringclass/stringClasses.java
@@ -0,0 +1,18 @@
+package stringclass;
+public class stringClasses {
+
+      public static void main(String[] args) {
+        String s1 = new String();
+        String s2 = new String();
+        stringClasses si = new stringClasses();
+        stringClasses si1 = new stringClasses();
+        String str1 = "Testing";
+          String str2 = "Testing";
+        if(si==si1){
+            System.out.println("Same");
+        }else{
+            System.out.println("not same");
+        }
+
+    }
+}
