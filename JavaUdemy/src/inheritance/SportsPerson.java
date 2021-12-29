e9632e465419435ab89bf9f7ff00476cdfea7955
harmeet2401 <harmeet2401@gmail.com>
2021-12-29 15:44:01 +0530

    .

--- JavaUdemy/src/inheritance/SportsPerson.java
+++ JavaUdemy/src/inheritance/SportsPerson.java
@@ -5,6 +5,9 @@ import com.sun.org.apache.xerces.internal.impl.xs.util.XSObjectListImpl;
 public class SportsPerson extends Human{
     public String playSoftBall;
 
+    public static void lesswalk(Human h){
+        System.out.println(h.name + ": walks less");
+    }
     public static void main(String[] args) {
         SportsPerson sportsPerson = new SportsPerson();
         sportsPerson.name="Dhoni";
@@ -14,6 +17,7 @@ public class SportsPerson extends Human{
         System.out.println(sportsPerson.name +" of age :"+ sportsPerson.age+
                 " and gender : "+ sportsPerson.gender +" play : "+ sportsPerson.playSoftBall);
         sportsPerson.speak();
+        lesswalk(sportsPerson);  //---------------------------------------------------------> subclass refenrce means HUman super set hai and sportsperson is subset humans ka
     }
     public  void speak(){
         System.out.println(name + ": can speak a faster way.");
