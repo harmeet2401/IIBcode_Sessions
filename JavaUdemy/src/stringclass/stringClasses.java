705a4707eaf1003774c321080cac187b4ecfa298
harmeet2401 <harmeet2401@gmail.com>
2021-12-29 17:58:10 +0530

    string buffer and string builder
    string buffer and string builder

--- JavaUdemy/src/stringclass/stringClasses.java
+++ JavaUdemy/src/stringclass/stringClasses.java
@@ -7,12 +7,28 @@ public class stringClasses {
         stringClasses si = new stringClasses();
         stringClasses si1 = new stringClasses();
         String str1 = "Testing";
+        str1 = str1.concat("-again");
+          System.out.println(str1 + str1.charAt(0)+ str1.split(",") );
           String str2 = "Testing";
         if(si==si1){
             System.out.println("Same");
         }else{
             System.out.println("not same");
         }
+          System.out.println("--------------------------------------------------------------------------------");
+          StringBuffer strb = new StringBuffer("Welcome to java ");
+          strb.append("((");
+          strb.reverse();
+          strb.insert(1,"get the old string ");
+
+          System.out.println(strb);
+
+          StringBuilder stringBuilder = new StringBuilder("Welcome to java ");
+          stringBuilder.append("@@");
+          stringBuilder.reverse();
+          stringBuilder.insert(2,"get the new strring ");
+          System.out.println(stringBuilder);
+
 
     }
 }
