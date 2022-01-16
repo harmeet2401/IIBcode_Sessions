b6ce356d26e795847164f42d4930538118a3c62e
harmeet2401 <harmeet2401@gmail.com>
2022-01-16 15:29:46 +0530

    Update practice.java

--- HackerrankPractice/src/practice.java
+++ HackerrankPractice/src/practice.java
@@ -25,7 +25,7 @@ public class practice {
        //CollectionsMethods();
        //feherenheitToCelcius();
        //sumOfIntegerDigits();
-        sumOfIntegerDigits();
+        minutesToYearsDays();
 
     }
 
@@ -305,8 +305,17 @@ public class practice {
         System.out.println("SUM : "+sum);
     }
     private static void minutesToYearsDays(){
-        Scanner in = new Scanner(System.in);
-        int minutes = in.nextInt();
-        
+        double minutesInYear = 60 * 24 * 365;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input the number of minutes: ");
+
+        double min = input.nextDouble();
+
+        long years = (long) (min / minutesInYear);
+        int days = (int) (min / 60 / 24) % 365;
+
+        System.out.println((int) min + " minutes is approximately " + years + " years and " + days + " days");
     }
 }
