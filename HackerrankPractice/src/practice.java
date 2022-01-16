96921c6d8c4c31a429c51ce7f74aeea598720c7a
harmeet2401 <harmeet2401@gmail.com>
2022-01-16 16:56:48 +0530

    lexicographically

--- HackerrankPractice/src/practice.java
+++ HackerrankPractice/src/practice.java
@@ -453,5 +453,27 @@ public class practice {
         System.out.println("Character(unicode point) = " + val1);
         // prints character at index9 in string
         System.out.println("Character(unicode point) = " + val2);
+
+        String str1 = "This is Exercise 1";
+        String str2 = "This is Exercise 2";
+        int result=str1.compareTo(str2);
+        if (result < 0)
+        {
+            System.out.println("\"" + str1 + "\"" +
+                    " is less than " +
+                    "\"" + str2 + "\"");
+        }
+        else if (result == 0)
+        {
+            System.out.println("\"" + str1 + "\"" +
+                    " is equal to " +
+                    "\"" + str2 + "\"");
+        }
+        else // if (result > 0)
+        {
+            System.out.println("\"" + str1 + "\"" +
+                    " is greater than " +
+                    "\"" + str2 + "\"");
+        }
     }
 }
