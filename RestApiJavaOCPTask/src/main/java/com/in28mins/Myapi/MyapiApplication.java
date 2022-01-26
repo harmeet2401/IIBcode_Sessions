e10848d2a911fcbf0f475de74b4fc9979a61286d
harmeet2401 <harmeet2401@gmail.com>
2022-01-26 22:16:41 +0530

    java session added
    java session added

--- RestApiJavaOCPTask/src/main/java/com/in28mins/Myapi/MyapiApplication.java
+++ RestApiJavaOCPTask/src/main/java/com/in28mins/Myapi/MyapiApplication.java
@@ -3,6 +3,7 @@ package com.in28mins.Myapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@EnableConfigServer //--- this will come only when we use the spring cloud starter dependencies
 @SpringBootApplication
 public class MyapiApplication {
 
