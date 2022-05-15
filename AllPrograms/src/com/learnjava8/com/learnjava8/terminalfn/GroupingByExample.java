036f66f61c96464720a8a49272df87575fef5600
harmeet2401 <harmeet2401@gmail.com>
2022-05-15 23:00:50 +0530

    Collectors
    Collectors

--- AllPrograms/src/com/learnjava8/com/learnjava8/terminalfn/GroupingByExample.java
+++ AllPrograms/src/com/learnjava8/com/learnjava8/terminalfn/GroupingByExample.java
@@ -3,14 +3,19 @@ package com.learnjava8.com.learnjava8.terminalfn;
 import com.learnjava8.bean.Students;
 import com.learnjava8.service.StudentInstace;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 public class GroupingByExample {
     public static void main(String[] args) {
         //System.out.println(groupByMarks());
-        System.out.println(groupByCustomizeKey());
+        //System.out.println(groupByCustomizeKey());
+       // groupingBy_1();
+        //groupingBy_2();
+        //groupingBy_3();
+        groupingBy_4();
 
     }
 
@@ -22,7 +27,7 @@ public class GroupingByExample {
     public static Map<Integer,List<Students>> groupByMarks(){
          Map<Integer,List<Students>> collect = StudentInstace.getstudents()
                 .stream()
-                .collect(Collectors.groupingBy(students -> students.getMarks()));
+                .collect(groupingBy(students -> students.getMarks()));
          return collect;
     }
 
@@ -34,7 +39,60 @@ public class GroupingByExample {
     public static Map<String,List<Students>> groupByCustomizeKey(){
         Map<String, List<Students>> mapstr = StudentInstace.getstudents()
                 .stream()
-                .collect(Collectors.groupingBy(students -> students.getMarks() > 600 ? "OUTSTANDING" : "AVERAGE"));
+                .collect(groupingBy(students -> students.getMarks() > 600 ? "OUTSTANDING" : "AVERAGE"));
         return mapstr;
     }
+
+    /**
+     * This fn have two level grouping
+     */
+    public  static void groupingBy_1()
+    {
+        Map<Integer, Map<String, List<Students>>> map  = StudentInstace.getstudents()
+                .stream()
+                .collect(groupingBy(Students::getMarks,groupingBy(Students::getName)));
+        System.out.println(map);
+    }
+    /**
+     * This fn have two level grouping
+     */
+    public  static void groupingBy_2()
+    {
+        Map<String, Integer> map  = StudentInstace.getstudents()
+                .stream()
+                .collect(groupingBy(Students::getName,Collectors.summingInt(Students::getNotebooks)));
+        System.out.println(map);
+
+    }
+    /**
+     * This fn have three level grouping
+     */
+    public  static void groupingBy_3()
+    {
+        Map<Integer, List<Students>> map  = StudentInstace.getstudents()
+                .stream()
+                .filter(students -> students.getMarks()>-500)
+                .collect(groupingBy(Students::getNotebooks, LinkedHashMap::new,Collectors.toList()));
+        System.out.println(map);
+
+    }
+    /**
+     * This fn have three level grouping
+     */
+    public  static void groupingBy_4()
+    {
+        Map<Integer, Students> map  = StudentInstace.getstudents()
+                .stream()
+                .filter(students -> students.getMarks()>=500)
+                .collect(groupingBy(Students::getNotebooks,collectingAndThen(maxBy(Comparator.comparing(Students::getMarks)),Optional::get)));
+        System.out.println(map);
+
+        Map<Integer, Students> map2  = StudentInstace.getstudents()
+                .stream()
+                .filter(students -> students.getMarks()>=500)
+                .collect(groupingBy(Students::getNotebooks,collectingAndThen(minBy(Comparator.comparing(Students::getMarks)),Optional::get)));
+        System.out.println(map2);
+
+    }
+
 }
---
+++ AllPrograms/src/com/learnjava8/com/learnjava8/terminalfn/PartitioningExample.java
@@ -0,0 +1,29 @@
+package com.learnjava8.com.learnjava8.terminalfn;
+
+import com.learnjava8.bean.Students;
+import com.learnjava8.service.StudentInstace;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static java.util.stream.Collectors.*;
+
+public class PartitioningExample {
+    public static void main(String[] args) {
+       // System.out.println(partitioningBY_1());
+        System.out.println(partitioningBY_2());
+    }
+    public static Map<Boolean, List<Students>> partitioningBY_1(){
+        Map<Boolean, List<Students>> collect = StudentInstace.getstudents()
+                .stream()
+                .collect(partitioningBy(Students -> Students.getMarks() >= 500));
+        return collect;
+    }
+    public static Map<Boolean, Set<Students>> partitioningBY_2(){
+        Map<Boolean, Set<Students>> collect = StudentInstace.getstudents()
+                .stream()
+                .collect(partitioningBy(Students -> Students.getMarks() >= 500,toSet()));
+        return collect;
+    }
+}
