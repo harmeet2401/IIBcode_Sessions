541d2817e25329396cbc747ca7887ddaf6640009
harmeet2401 <53974957+harmeet2401@users.noreply.github.com>
2022-10-28 17:53:55 +0530

    Security added to the api using
    Security added to the api using SecurityFilterChain class

---
+++ rest-webservice/src/main/java/com/in28minutes/rest/webservices/restfullservice/security/SpringSecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.in28minutes.rest.webservices.restfullservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+
+        httpSecurity.authorizeHttpRequests().anyRequest().authenticated();
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        httpSecurity.csrf().disable();
+        return httpSecurity.build();
+    }
+}
