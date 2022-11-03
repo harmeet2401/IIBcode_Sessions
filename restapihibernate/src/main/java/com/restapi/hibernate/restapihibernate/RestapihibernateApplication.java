58ca041cf7b78b3a21219f3fd501dd10ba78c2a7
IBMADMIN <53974957+harmeet2401@users.noreply.github.com>
2022-11-03 18:07:53 +0530

    hibernate-jdbc-jpa
    hibernate-jdbc-jpa

---
+++ restapihibernate/src/main/java/com/restapi/hibernate/restapihibernate/RestapihibernateApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.hibernate.restapihibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapihibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapihibernateApplication.class, args);
+	}
+
+}
---
+++ restapihibernate/src/test/java/com/restapi/hibernate/restapihibernate/RestapihibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.hibernate.restapihibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapihibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
