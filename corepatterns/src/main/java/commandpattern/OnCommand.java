3b49f47cc4f656ae9525f0e9d91d04e9c2a495b5
IBMADMIN <53974957+harmeet2401@users.noreply.github.com>
2022-12-14 22:20:27 +0530

    Command design pattern
    Command design pattern

---
+++ corepatterns/src/main/java/commandpattern/OnCommand.java
@@ -0,0 +1,12 @@
+package commandpattern;
+
+public class OnCommand implements Command{
+    Television television;
+    public OnCommand(Television television){
+        this.television=television;
+    }
+    @Override
+    public void execute() {
+        television.on();
+    }
+}
---
+++ corepatterns/src/main/java/commandpattern/Television.java
@@ -0,0 +1,9 @@
+package commandpattern;
+public class Television {
+    public void on(){
+        System.out.println("Television switched on");
+    }
+    public void off(){
+        System.out.println("Television switched off");
+    }
+}
