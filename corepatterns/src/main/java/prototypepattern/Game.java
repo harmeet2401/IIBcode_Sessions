08223a8e794b7b9991f1d76a597476cd0af424a6
IBMADMIN <53974957+harmeet2401@users.noreply.github.com>
2022-12-17 00:42:57 +0530

    Shallow and deep copy
    Shallow and deep copy

--- corepatterns/src/main/java/prototypepattern/Game.java
+++ corepatterns/src/main/java/prototypepattern/Game.java
@@ -3,10 +3,22 @@ package prototypepattern;
 public class Game  implements  Cloneable{
     private int id;
     private String name;
+    private Membership membership;
 
     @Override
     protected Game clone() throws CloneNotSupportedException {
-        return (Game) super.clone();
+        Game game = (Game) super.clone();
+        game.setMembership(new Membership());
+        return game;
+    }
+
+    Game(){
+
+    }
+    Game(Game game){
+        this.id = game.id;
+        this.name = game.name;
+        this.membership= new Membership();
     }
 
     @Override
@@ -14,7 +26,16 @@ public class Game  implements  Cloneable{
         return "Game{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", membership=" + membership +
                 '}';
+    }
+
+    public Membership getMembership() {
+        return membership;
+    }
+
+    public void setMembership(Membership membership) {
+        this.membership = membership;
     }
 
     public int getId() {
--- corepatterns/src/main/java/prototypepattern/Test.java
+++ corepatterns/src/main/java/prototypepattern/Test.java
@@ -4,17 +4,21 @@ package prototypepattern;
  * Below example shows how we can clone the prototype object game1 to create a new
  * object game2 without setting Id and name
  * And we can see that hash codes are different so new object is created with same
- * attribute values
+ * attribute values , as it is a shallow copy which is the default behaviour of the
+ * clone function it can copy same reference pointer and can have same hashcode.
  */
 public class Test {
     public static void main(String[] args) throws CloneNotSupportedException {
         Game game1 = new Game();
         game1.setId(1);
         game1.setName("GOW");
+        game1.setMembership(new Membership());
 
         //using clone functions
         Game game2 = game1.clone();
+        Game game3 = new Game(game1);
         System.out.println(game1 +" hash values"+game1.hashCode());
         System.out.println(game2 +" hash values"+game2.hashCode());
+        System.out.println(game3 +" hash values"+game3.hashCode());
     }
 }
