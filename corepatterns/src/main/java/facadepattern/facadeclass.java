302009460ca715c4f4d16e62937d1adcf23c8ffb
IBMADMIN <53974957+harmeet2401@users.noreply.github.com>
2022-12-17 21:28:05 +0530

    Update facadeclass.java

--- corepatterns/src/main/java/facadepattern/facadeclass.java
+++ corepatterns/src/main/java/facadepattern/facadeclass.java
@@ -1,9 +1,6 @@
 package facadepattern;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 public class facadeclass {
     /**
@@ -26,14 +23,27 @@ public class facadeclass {
         stringBuffer.append(orderProcessing.deliverOrder(name));
 
         List<String> list = new ArrayList<>();
+        Map<Integer,String> map = new HashMap<>();
+
         for (int i = 0; i <10; i++) {
             list.add(Integer.toString(i));
+            map.put(i,"String : "+Integer.toString(i));
         }
-        Iterator<String> iterator = list.iterator();
+        Iterator<String> listiterator = list.iterator();
 
-        while (iterator.hasNext()){
+        while (listiterator.hasNext()){
             stringBuffer.append("\n");
-            stringBuffer.append(iterator.next());
+            stringBuffer.append(listiterator.next());
+        }
+
+        Set<Integer> mapset = map.keySet();
+        Collection<String> mapset1 = map.values();
+        Iterator<Integer> mapiterator = mapset.iterator();
+        Iterator<String> mapiterator1 = mapset1.iterator();
+        System.out.println("Set printing.:-----------------");
+        while (mapiterator.hasNext()){
+            System.out.println("Set key : "+mapiterator.next());
+            System.out.println("Set value : "+mapiterator1.next());
         }
         return stringBuffer.toString();
     }
