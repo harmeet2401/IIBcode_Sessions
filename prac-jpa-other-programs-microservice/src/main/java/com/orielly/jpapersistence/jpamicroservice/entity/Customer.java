010a700bf85f3f55a4e600ca7ccd95b0a5f5261b
harmeet2401 <53974957+harmeet2401@users.noreply.github.com>
2024-03-20 12:27:21 +0530

    Update Customer.java

--- prac-jpa-other-programs-microservice/src/main/java/com/orielly/jpapersistence/jpamicroservice/entity/Customer.java
+++ prac-jpa-other-programs-microservice/src/main/java/com/orielly/jpapersistence/jpamicroservice/entity/Customer.java
@@ -8,6 +8,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Objects;
 
 
 @Entity(name = "Test")
@@ -64,6 +65,19 @@ public class Customer {
 
     public void setaddress(String ADDRESS) {
         this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return id == customer.id && fname.equals(customer.fname) && lname.equals(customer.lname) && address.equals(customer.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, fname, lname, address);
     }
 
     @Override
